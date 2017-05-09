d624c980a0a4042da7bf6b753bfe31dd2a73621c
Ahmed Mohamed Magdi <ahmed.moh.magdi93@gmail.com>
2017-05-09 11:07:48 +0200

    changing saveToken to public + ResponseStatus to public

--- RxBaseProject/Classes/Sources/Models/BaseTokenModel.swift
+++ RxBaseProject/Classes/Sources/Models/BaseTokenModel.swift
@@ -43,7 +43,7 @@ public class BaseTokenModel: BaseModel {
     
     //MARK:- Model Coding Managing
     
-    func save() {
+    public func save() {
         self.store(self, withKey: "current_session")
     }
     
